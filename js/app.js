b8447a0aabc3427422032fabc53eb5a22c8b648b
Twilight Courier <twilightcourier@proton.me>
2026-04-26 17:27:51 +0300

    added header_navbuttons, if fix white space background

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 const hamMenu = document.querySelector(".menu-icon");
 
-const offScreenMenu = document.querySelector(".nav-header-items");
+
+
+const offScreenMenu = document.querySelector(".header-items");
+
+document.addEventListener("DOMContentLoaded", function() {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+
+});
+
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
